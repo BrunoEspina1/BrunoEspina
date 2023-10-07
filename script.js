bc3bc9020cd0378c01750827f55df2a856f175f3
Supergoge <brunoalberto.espina.ce145@gmail.com>
2023-10-07 16:15:19 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//Variables
+var skillsButton = document.getElementById("skills-button");
+var educationButton = document.getElementById("education-button");
+var skillsSection = document.getElementById("skills");
+var educationSection = document.getElementById("education");
+
+/*Animation from the home text*/
+var typed = new Typed(".typing", {
+    strings:["Front-end Developer", "Web Developer", "Software Engineering Student"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+
+}) 
