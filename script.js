ed26877fc0a3349ec2f5d9f0b45227395c6bb7b4
Supergoge <brunoalberto.espina.ce145@gmail.com>
2023-10-08 14:22:04 -0600

    Various Changes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//Variables
-var skillsButton = document.getElementById("skills-button");
-var educationButton = document.getElementById("education-button");
-var skillsSection = document.getElementById("skills");
-var educationSection = document.getElementById("education");
-
 /*Animation from the home text*/
-var typed = new Typed(".typing", {
-    strings:["Front-end Developer", "Web Developer", "Software Engineering Student"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop: true
-
-}) 
+var typedf = new Typed(".home-first-line-text-typing",{
+    stringsElement: "#home-first-line-text",
+    typeSpeed: 50,
+    BackSpeed: 0,
+    showCursor: false
+})
+var typeds = new Typed(".home-second-line-text-typing",{
+    startDelay: 3000,
+    stringsElement: "#home-second-line-text",
+    typeSpeed: 50,
+    BackSpeed: 50,
+    showCursor: false
+})
